0aa545261f4fc8dec6eda4904b5cc41d08418667
kain <zhangtqx@vip.qq.com>
2020-08-30 15:52:47 +0800

    reset

--- app/types/tunnel_option.go
+++
@@ -1,8 +0,0 @@
-package types
-
-type TunnelOption struct {
-	SrcIp   string `yaml:"src_ip"`
-	SrcPort uint32 `yaml:"src_port"`
-	DstIp   string `yaml:"dst_ip"`
-	DstPort uint32 `yaml:"dst_port"`
-}
--- app/utils/get_addr.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "strconv"
-
-// Get Addr
-func GetAddr(ip string, port uint) string {
-	return ip + ":" + strconv.Itoa(int(port))
-}
