4eb3d69afb811c69f8e81a58d056c7308a357ec7
kain <zhangtqx@vip.qq.com>
2020-12-01 16:12:33 +0800

    update

--- application/service/transfer/transfer_test.go
+++ application/service/transfer/transfer_test.go
@@ -11,7 +11,7 @@ import (
 	"testing"
 )
 
-var es *Elastic
+var transfer *Transfer
 
 func TestMain(m *testing.M) {
 	os.Chdir("../../../")
@@ -30,7 +30,7 @@ func TestMain(m *testing.M) {
 	if err = yaml.Unmarshal(bs, &cfg); err != nil {
 		return
 	}
-	if es, err = New(&Dependency{
+	if transfer, err = New(&Dependency{
 		Config: cfg,
 		Schema: schema.New("./config/autoload/"),
 	}); err != nil {
@@ -40,7 +40,7 @@ func TestMain(m *testing.M) {
 }
 
 func TestElasticManager_Put(t *testing.T) {
-	err := es.Put(options.PipeOption{
+	err := transfer.Put(options.PipeOption{
 		Identity: "debug",
 		Index:    "debug-log",
 		Validate: `{"type":"object","properties":{"name":{"type":"string"}}}`,
@@ -53,7 +53,7 @@ func TestElasticManager_Put(t *testing.T) {
 }
 
 func TestElasticManager_Delete(t *testing.T) {
-	err := es.Delete("debug")
+	err := transfer.Delete("debug")
 	if err != nil {
 		t.Error(err)
 	}
